d4d7f53d5b68f45a2c9dcb7133bd2be3d2a2bc4a
vjudge <gradonday@gmail.com>
2022-01-20 17:34:52 +0800

    add global spec

--- extension-libs/ts-jest/jest-sample/global.spec.ts
+++ extension-libs/ts-jest/jest-sample/global.spec.ts
@@ -66,5 +66,107 @@ describe('describe', () => {
     })
 })
 
+describe.each([
+    [1, 1, 2],
+    [1, 2, 3],
+    [2, 1, 3],
+])('.add(%i, %i)', (a, b, expected) => {
+    test(`[1] returns ${expected}`, () => {
+        expect(a + b).toBe(expected);
+    });
+
+    test(`[1] returned value not be greater than ${expected}`, () => {
+        expect(a + b).not.toBeGreaterThan(expected);
+    });
+
+    test(`[1] returned value not be less than ${expected}`, () => {
+        expect(a + b).not.toBeLessThan(expected);
+    });
+});
+
+
+describe.skip.each([
+    {a: 1, b: 1, expected: 2},
+    {a: 1, b: 2, expected: 3},
+    {a: 2, b: 1, expected: 3},
+])('.add($a, $b)', ({a, b, expected}) => {
+    test(`[2] returns ${expected}`, () => {
+        expect(a + b).toBe(expected);
+    });
+
+    test(`[2] returned value not be greater than ${expected}`, () => {
+        expect(a + b).not.toBeGreaterThan(expected);
+    });
+
+    test(`[2] returned value not be less than ${expected}`, () => {
+        expect(a + b).not.toBeLessThan(expected);
+    });
+});
+
+
+describe.each`
+  a    | b    | expected
+  ${1} | ${1} | ${2}
+  ${1} | ${2} | ${3}
+  ${2} | ${1} | ${3}
+`('$a + $b', ({a, b, expected}) => {
+    test(`[3] returns ${expected}`, () => {
+        expect(a + b).toBe(expected);
+    });
+
+    test(`[3] returned value not be greater than ${expected}`, () => {
+        expect(a + b).not.toBeGreaterThan(expected);
+    });
+
+    test(`[3] returned value not be less than ${expected}`, () => {
+        expect(a + b).not.toBeLessThan(expected);
+    });
+});
+
+// 只运行这一个描述块中的test用例，其他的 describe 会被忽略
+describe.only('only-1', () => {
+    test('is true', () => {
+        expect(true).toBeTruthy();
+        console.log('true')
+    });
+
+    test('is false', () => {
+        expect(false).toBeFalsy();
+        console.log('false')
+    });
+});
+
+
+describe.only.each([
+    [1, 1, 2],
+    [1, 2, 3],
+    [2, 1, 3],
+])('.add(%i, %i)', (a, b, expected) => {
+    test(`[only.each] returns ${expected}`, () => {
+        expect(a + b).toBe(expected);
+    });
+});
+
+
+test.concurrent.each([
+    [1, 1, 2],
+    [1, 2, 3],
+    [2, 1, 3],
+])('.add(%i, %i)', async (a, b, expected) => {
+    expect(a + b).toBe(expected);
+});
+
+
+test.concurrent.each`
+  a    | b    | expected
+  ${1} | ${1} | ${2}
+  ${1} | ${2} | ${3}
+  ${2} | ${1} | ${3}
+`('returns $expected when $a is added $b', async ({a, b, expected}) => {
+    expect(a + b).toBe(expected);
+});
+
+
+
 
 
