9ecf304071093355be3bf145fb7fa5b7028a2953
vjudge <gradonday@gmail.com>
2022-01-20 15:54:07 +0800

    add before after spec

--- extension-libs/ts-jest/jest-sample/before-after.spec.ts
+++ extension-libs/ts-jest/jest-sample/before-after.spec.ts
@@ -17,11 +17,54 @@ afterEach(() => {
 
 test('each', () => {
     expect(sum(1, 2)).toBe(3);
+    console.log('[1] expect: beforeEach & afterEach');
+})
+
+test('each', () => {
+    expect(sum(1, 2)).toBe(3);
+    console.log('[2] expect: beforeEach & afterEach');
 })
 
 
+// beforeAll + afterAll
+beforeAll(() => {
+    printMsg('All');
+})
+
+afterAll(() => {
+    printMsg('All', 'after');
+})
 
 
+describe('describe', () => {
+    // beforeEach + afterEach
+    beforeEach(() => {
+        printMsg('Each', '[describe]: before');
+    })
+
+    afterEach(() => {
+        printMsg('Each', '[describe]: after');
+    })
+
+    test('each', () => {
+        expect(sum(1, 2)).toBe(3);
+        console.log('[describe:1] expect: beforeEach & afterEach');
+    })
+
+    test('each', () => {
+        expect(sum(1, 2)).toBe(3);
+        console.log('[describe:2] expect: beforeEach & afterEach');
+    })
+
+// beforeAll + afterAll
+    beforeAll(() => {
+        printMsg('All', '[describe]: before');
+    })
+
+    afterAll(() => {
+        printMsg('All', '[describe]: after');
+    })
+})
 
 
 
