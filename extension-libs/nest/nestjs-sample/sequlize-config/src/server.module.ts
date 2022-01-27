7c3542ff3c27f3d886111d9756e29c8ffda877e4
vjudge <gradonday@gmail.com>
2022-01-27 12:13:07 +0800

    add server module

--- extension-libs/nest/nestjs-sample/sequlize-config/src/server.module.ts
+++ extension-libs/nest/nestjs-sample/sequlize-config/src/server.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
 
 import { ConfigModule } from './config/config.module';
-import { DbModule } from './modules/sequelize/db.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { UserModule } from './modules/user/user.module';
+import { DbModule } from './sequelize/db.module';
 
 @Module({
   imports: [
     ConfigModule.register({ folder: './config' }),
-    HttpModule.register({
-      timeout: 5000,
-    }),
-    DbModule,
-    AuthModule,
-    UserModule
+    DbModule
   ],
   providers: [
   ],
